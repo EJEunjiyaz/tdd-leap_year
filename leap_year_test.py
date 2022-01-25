2fc988c90544e964385af50f93712f4be33fec6a
Vichyawat Nakarugsa <prince_vichyawat@windowslive.com>
2022-01-25 18:40:11 +0700

    Add test cases 2-4

--- leap_year_test.py
+++ leap_year_test.py
@@ -1,7 +1,12 @@
 import unittest
 from leap_year import leap_year
 
-test_cases = [('1', False)]
+test_cases = [
+    ('1', False),
+    ('2', False),
+    ('3', False),
+    ('4', True)
+]
 
 
 class LeapYearTestClass(unittest.TestCase):
