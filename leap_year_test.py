a5091ebbbfbe29e66c33df50760bc92fa20166c2
Vichyawat Nakarugsa <prince_vichyawat@windowslive.com>
2022-01-25 18:56:33 +0700

    Adjust string to int

--- leap_year_test.py
+++ leap_year_test.py
@@ -2,10 +2,10 @@ import unittest
 from leap_year import leap_year
 
 test_cases = [
-    ('1', "1 is not leap year."),
-    ('2', "2 is not leap year."),
-    ('3', "3 is not leap year."),
-    ('4', "4 is leap year.")
+    (1, "1 is not leap year."),
+    (2, "2 is not leap year."),
+    (3, "3 is not leap year."),
+    (4, "4 is leap year.")
 ]
 
 
